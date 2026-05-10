ae43e75858f013a2f63e792aa742d063df9a88e0
vitorma28 <vimat28.3.1@gmail.com>
2026-05-09 21:25:54 -0300

    Criados arquivos 'conceitos.md' para entidades e conceitos necessário para a teoria do programa, e 'readme.md' para descrição geral do projeto.

--- lista.js
+++ lista.js
@@ -3,19 +3,18 @@ class ItemDAO {
 }
 
 class Item {
-    nome
-    preço
-    quantidade
+    nome;
+    preço;
+    quantidade;
 
     constructor(nome, preço, quantidade) {
-
-        this.nome = nome
-        this.preço = preço
-        this.quantidade = quantidade
+        this.nome = nome;
+        this.preço = preço;
+        this.quantidade = quantidade;
     }
 
     valorTotal() {
-        return this.preço * this.quantidade
+        return this.preço * this.quantidade;
     }
 }
 
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Gerenciador de Feira
+
+## Descrição
+
+É um `CRUD` simples feito para gerenciar compras feitas em uma feira.
