3e62dbcc6442380d20359c14bf5832803c24722b
vitorma28 <vimat28.3.1@gmail.com>
2026-05-16 17:54:03 -0300

    Quase criação da interface
    Código das opções criado, falta só executar com base em opção.

--- Interface.js
+++ Interface.js
@@ -1,7 +1,80 @@
-import { Item } from "./Item";
-import { ItemDAO } from "./ItemDAO";
+import { Item } from "./Item.js";
+import { ItemDAO } from "./ItemDAO.js";
+import PromptSync from 'prompt-sync';
+const prompt = PromptSync();
 
 
 export class Interface {
-        // TODO
+        sair = false;
+        dao;
+
+        constructor(dao) {
+                this.dao = dao;
+        }
+
+        static async build(banco) {
+                const i = new Interface(ItemDAO.build(banco));
+
+                return i;
+        }
+
+
+        #menu() {
+                const menu =
+`/---
+| 0. Sair do programa.
+| 1. Lista todas as compras.
+| 2. Acessar uma compra específica.
+| 3. Atualizar uma compra.
+| 4. Inserir novo item.
+| 5. Remover item.
+\\---`
+                ;
+                console.log(menu);
+        }
+
+        #opcoes(opcao) {
+                switch (opcao) {
+                        case 0: {
+                                this.sair = true;
+
+                                break;
+                        }
+                        case 1: {
+                                break;
+                        }
+                        case 2: {
+                                break;
+                        }
+                        case 3: {
+                                break;
+                        }
+                        case 4: {
+                                break;
+                        }
+                        case 5: {
+                                break;
+                        }
+                }
+        }
+
+        #loop() {
+                this.#menu();
+                console.log();
+                let opcao = prompt('> ');
+                
+                if (!/^\d+$/.test(opcao)) {
+                        return;
+                }
+
+                opcao = parseInt(opcao);
+
+                this.#opcoes(opcao);
+        }
+
+        start() {
+                while (!this.sair) {
+                        this.#loop();
+                }
+        }
 }
---
+++ index.js
@@ -0,0 +1,9 @@
+import { Interface } from "./Interface.js";
+
+function main() {
+        const i = new Interface();
+
+        i.start();
+}
+
+main();
